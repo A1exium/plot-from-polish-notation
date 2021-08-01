fdb012f892833a871dac5c8d52a4de66a9b8b73a
Dominga Misty <domisty@ge-d5.nsk.21-school.ru>
2021-08-01 16:31:57 +0700

    final fix

--- src/tests/test_eval.c
+++ src/tests/test_eval.c
@@ -2,13 +2,17 @@
 #include "parsing.h"
 #include "str.h"
 
-void TestEval_1() {
-  printf("Testing Eval: test 1\n");
-  char example[] = "1 2 + 4 * 3 +";
+const double kZero = 1E-6;
+
+void TestEval(int num, char *example, double x, double expect) {
+  printf("Testing Eval: test %d\n", num);
   printf("in: {%s}\n", example);
-  double ans = Eval(example, 1);
+  double ans = Eval(example, x);
   printf("out: {%lf}\n", ans);
-  Assert(ans, 15);
+  Assert(ans - expect <= kZero, 1);
 }
 
-int main(void) { TestEval_1(); }
+int main(void) { 
+  TestEval(1, "1 2 + 4 * 3 +", 1, 15);
+  TestEval(2, "5 3 ^ 5 5 * / 10 +", 5, 15);
+}
