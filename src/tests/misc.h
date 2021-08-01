9aabe12658d6d41c6472b0cae0d5b238682b0b7a
Dominga Misty <domisty@ge-d5.nsk.21-school.ru>
2021-08-01 15:55:23 +0700

    add tests

---
+++ src/tests/misc.h
@@ -0,0 +1,1 @@
+void Assert(int a, int b);
---
+++ src/tests/test_eval.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "parsing.h"
+#include "misc.h"
+#include "str.h"
+
+void TestEval_1() {
+  printf("Testing Eval: test 1\n");
+  char example[] = "1 2 + 4 * 3 +";
+  printf("in: {%s}\n", example);
+  double ans = Eval(example, 1);
+  printf("out: {%lf}\n", ans);
+  Assert(ans, 15);
+}
+
+int main(void) {
+  TestEval_1();
+}
